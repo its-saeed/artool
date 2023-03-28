505d7feadb78c6602fe9478c4d01cacb4d72ff6d
Saeed Dadkhah <saeed@zilliqa.com>
2023-03-28 13:18:30 +0330

    Minimal working version

---
+++ README.md
@@ -0,0 +1,19 @@
+# Artool
+This tool is proposed by Artem and supposed to help him. That's why it's called Artool :D
+
+# How to add new log type
+Logs is expected to be like this:
+```log
+[7f3606ffd6c0][21-03-22T10:18:49.218][EvmClient.cpp:185   ][CallRunner          ] ARTEM_SAEED DUMMY_LOG this is a dummy log 34.21.222.37 just for testing
+```
+
+We have `ARTEM_SAEED` as the magic phrase. Then `log_type` should be present. `log_type` is supposed to have only uppercase letters and `_` . For example here `DUMMY_LOG` is the log type.
+
+1. Define your log type like what we did already:
+```python
+LOG_TYPE_INTRO = "INTRO"
+LOG_TYPE_DNS_TEST = "DNS_TEST"      # For testing this script
+LOG_TYPE_DUMMY = "DUMMY"      # For testing this script
+```
+2. let's supposed you want to add `foo_log`. You need to define a function named `process_foo_log`. Do whatever you want to do with these class of logs in the function.
+3. Don't forget to add this function and it's `elif` entry in `process_line` function.
--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import os
 import re
+from datetime import datetime
 
 MAGIC = "ARTEM_SAEED"
 
 LOG_TYPE_INTRO = "INTRO"
 LOG_TYPE_DNS_TEST = "DNS_TEST"      # For testing this script
+LOG_TYPE_DUMMY = "DUMMY"      # For testing this script
 DNS_TABLE = {}
-FILTERED_LOGS = {}
+FILTERED_LOGS = []
 
 def parse_line(line):
     line_pattern = "(?:\[[^\]]*\])(?:\[(?P<timestamp>[^\]]*)\])(?:\[[^\]]*\])(?:\[[^\]]*\]) ARTEM_SAEED (?P<log_type>[A-Z_]+) (?P<log>.*)"
@@ -34,6 +36,8 @@ def process_line(parsed_line):
         process_intro_log(parsed_line)
     elif log_type == LOG_TYPE_DNS_TEST:
         process_dns_test_log(parsed_line)
+    elif log_type == LOG_TYPE_DUMMY:
+        process_dummy_log(parsed_line)
     else:
         raise ValueError(f"Log type {log_type} is unknown")
 
@@ -49,24 +53,62 @@ def process_intro_log(parsed_line):
         raise ValueError("{LOG_TYPE_INTRO} is malformed", )
 
 def process_dns_test_log(parsed_line):
-    intro_pattern = "this is for dns test (?P<ip>[\d\.]+)"
-    matched = re.match(intro_pattern, parsed_line["log"])
-    print(matched.group("ip"))
-    ip = matched.group("ip")
-    parsed_line["log"] = parsed_line["log"].replace(ip, DNS_TABLE[ip])
+    log = parsed_line["log"]
+    dns_test_pattern = "this is for dns test (?P<ip>[\d\.]+)"
+    matched = re.match(dns_test_pattern, parsed_line["log"])
+    if matched and "ip" in matched.groupdict():
+        translate_ip(parsed_line, matched.group("ip"))
+    else:
+        raise ValueError(f"DNS test message is not parsable, {log}")
+
+def process_dummy_log(parsed_line):
+    #this is a dummy log 33.33.36.37 just for testing
+    log = parsed_line["log"]
+    dummy_pattern = "this is a dummy log (?P<ip>[\d\.]+) just for testing"
+    matched = re.match(dummy_pattern, log)
+    if matched and "ip" in matched.groupdict():
+        translate_ip(parsed_line, matched.group("ip"))
+    else:
+        raise ValueError(f"Dummy message is not parsable, {log}")
+
+def translate_ip(parsed_line, extracted_ip):
+    if extracted_ip not in DNS_TABLE:
+        parsed_line["needs_ip_resolve"] = True # let's try at the end to resolve this ip to address
+        parsed_line["extracted_ip"] = extracted_ip # let's try at the end to resolve this ip to address
+        print(f"{extracted_ip} is not in the DNS table yet")
+    else:
+        parsed_line["log"] = parsed_line["log"].replace(extracted_ip, DNS_TABLE[extracted_ip])
+
 
 def process_file(filename):
-    os.system(f'grep {MAGIC} {filename} > /tmp/{filename}')
-    with open(f"/tmp/{filename}") as file:
+    temp_filename = "zil.log"
+    os.system(f'grep {MAGIC} {filename} > /tmp/{temp_filename}')
+    with open(f"/tmp/{temp_filename}") as file:
         for line in file:
             try:
                 parsed_line = parse_line(line.rstrip())
                 process_line(parsed_line)
-                FILTERED_LOGS[parsed_line["timestamp"]] = parsed_line["log"]
+                FILTERED_LOGS.append(parsed_line)
             except ValueError as err:
                 print(err.args)
 
-    print(FILTERED_LOGS)
+def process_folder(rootdir):
+    for subdir, dirs, files in os.walk(rootdir):
+        for file in files:
+            filename = os.path.join(subdir, file)
+            print("Processing", filename)
+            process_file(filename)
+
+    # Last try to resolve IP addresses
+    for parsed in FILTERED_LOGS:
+        if "needs_ip_resolve" in parsed:
+            translate_ip(parsed, parsed["extracted_ip"])
+        
+    # Sort based on timestamp
+    filtered_sorted = sorted(FILTERED_LOGS, key=lambda x: datetime.strptime(x["timestamp"], '%y-%m-%dT%H:%M:%S.%f'))
+
+    for log in filtered_sorted:
+        print(log["timestamp"], log["log"])
 
 if __name__ == "__main__":
-    process_file("sample.log")
+    process_folder("sample_logs")
